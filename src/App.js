9f43980ae6aa0f8dc45cb892f59268d142cf16f0
unknown <Shikder-Vai>
2022-05-11 01:05:53 +0600

    to many components added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Authentication/Login/Login";
+import SignUp from "./components/Authentication/SignUp/SignUp";
 import Home from "./components/HomePage/Home/Home";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signUp" element={<SignUp></SignUp>}></Route>
       </Routes>
     </div>
   );
