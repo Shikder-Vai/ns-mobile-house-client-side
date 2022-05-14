48f303b8a19880eac294052c24b302db1882e45c
unknown <Shikder-Vai>
2022-05-14 21:53:20 +0600

    some update

--- src/components/HomePage/Header/Header.js
+++ src/components/HomePage/Header/Header.js
@@ -1,17 +1,13 @@
-import { signOut } from "firebase/auth";
-import { useAuthState } from "react-firebase-hooks/auth";
-import auth from "../../../firebase.init";
-import { Button, Navbar } from "flowbite-react";
+// import { signOut } from "firebase/auth";
+// import { useAuthState } from "react-firebase-hooks/auth";
+// import auth from "../../../firebase.init";
+// import { Button, Navbar } from "flowbite-react";
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+// import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
-  const [user] = useAuthState(auth);
-  const handleLogOut = () => {
-    signOut(auth);
-  };
   return (
-    <Navbar fluid={true} rounded={true}>
+    /**<Navbar fluid={true} rounded={true}>
       <Navbar.Brand>
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
           Ns Mobile House
@@ -23,21 +19,9 @@ const Header = () => {
         <NavLink to={"/"}></NavLink>
         <NavLink to={"/home"}>Home</NavLink>
         <NavLink to={"/signUp"}></NavLink>
-        {user ? (
-          <Button className="button-style" onClick={handleLogOut}>
-            SIGN OUT
-          </Button>
-        ) : (
-          <NavLink
-            as={Link}
-            to="/login"
-            className={({ isActive }) => (isActive ? "link-active" : "link")}
-          >
-            LOGIN
-          </NavLink>
-        )}
       </Navbar.Collapse>
-    </Navbar>
+    </Navbar>**/
+    <div></div>
   );
 };
 
--- src/components/HomePage/NavBar/NavBar.js
+++ src/components/HomePage/NavBar/NavBar.js
@@ -1,59 +1,76 @@
 import React, { useState } from "react";
-import { XIcon } from '@heroicons/'
-import { NavLink } from "react-router-dom";
+import { MenuIcon, XIcon } from "@heroicons/react/solid";
+import { Link, NavLink } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+import { signOut } from "firebase/auth";
 
 const NavBar = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [user] = useAuthState(auth);
+  const handleLogOut = () => {
+    signOut(auth);
+  };
   return (
-    <nav className="bg-indigo-200 p-2">
-      <div onClick={() => setOpen(!open)} className="w-8 h-9 md:hidden">
-        {open ?  : <MenuIcon></MenuIcon>}
+    <nav className="bg-indigo-200 flex space-x-96 p-3 justify-center">
+      <div className=" flex items-start">
+        <button className=" font-serif font-bold text-orange-400 text-2xl">
+          NS MOBILE HOUSE
+        </button>
       </div>
-      <div
-        className={`text-2xl md:flex font-serif justify-center w-full bg-indigo-200 absolute md:static duration-500 ease-in ${
-          open ? "top-10" : "top-[-130px]"
-        }`}
-      >
-        <NavLink
-          className={({ isActive }) =>
-            isActive
-              ? " text-teal-500 font-bold mr-5"
-              : "mr-5 text-blue-900 font-semibold"
-          }
-          to={"/"}
+      <div>
+        <div onClick={() => setOpen(!open)} className="w-8 h-9 md:hidden">
+          {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+        </div>
+        <div
+          className={`text-2xl md:flex font-serif justify-center w-full bg-indigo-200 absolute md:static duration-500 ease-in ${
+            open ? "top-10" : "top-[-130px]"
+          }`}
         >
-          Home
-        </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            isActive
-              ? " text-teal-500 font-bold mr-5"
-              : "mr-5 text-blue-900 font-semibold"
-          }
-          to={"/reviews"}
-        >
-          Reviews
-        </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            isActive
-              ? " text-teal-500 font-bold mr-5"
-              : "mr-5 text-blue-900 font-semibold"
-          }
-          to={"/dashboard"}
-        >
-          Dashboard
-        </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            isActive
-              ? " text-teal-500 font-bold mr-5"
-              : "mr-5 text-blue-900 font-semibold"
-          }
-          to={"/blogs"}
-        >
-          Blogs
-        </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? " text-teal-500 font-bold mr-5"
+                : "mr-5 text-blue-900 font-semibold"
+            }
+            to={"/"}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? " text-teal-500 font-bold mr-5"
+                : "mr-5 text-blue-900 font-semibold"
+            }
+            to={"/blogs"}
+          >
+            Blogs
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? " text-teal-500 font-bold mr-5"
+                : "mr-5 text-blue-900 font-semibold"
+            }
+            to={"/blogs"}
+          >
+            About
+          </NavLink>
+          {user ? (
+            <button className="button-style" onClick={handleLogOut}>
+              SIGN OUT
+            </button>
+          ) : (
+            <NavLink
+              as={Link}
+              to="/login"
+              className={({ isActive }) => (isActive ? "link-active" : "link")}
+            >
+              LOGIN
+            </NavLink>
+          )}
+        </div>
       </div>
     </nav>
   );
